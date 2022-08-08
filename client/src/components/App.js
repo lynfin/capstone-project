3c803959f082ba5430b69b06b91ab50605127d9c
Lyn <lynfinman@gmail.com>
2022-08-08 17:06:18 -0400

    cleaned out navbar and newworkout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import NavBar from './NavBar';
 import Login from './Login';
 import WorkoutList from './WorkoutList';
-import NewWorkout from './NewWorkout';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -17,18 +15,12 @@ function App() {
     });
   }, []);
 
-  //if (!user) return <Login onLogin={setUser} />;
-
   return (
     <>
-      <NavBar user={user} setUser={setUser} />
       <main>
         <Switch>
           <Route exact path='/login'>
             <Login onLogin={setUser} />
-          </Route>
-          <Route exact path='/new'>
-            <NewWorkout user={user} />
           </Route>
           <Route exact path='/'>
             <WorkoutList />
